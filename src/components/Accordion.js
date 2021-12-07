083dd654d63205217eb0fa8caf2383ce2addadec
Travis Mathews <xenoaurora@gmail.com>
2021-12-07 08:51:53 -0500

    Added active class to accordion elements

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -11,13 +11,15 @@ const Accordion = ({ items }) => {
 
 
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClick(index)}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
             <p>{item.content}</p>
         </div>
       </React.Fragment>
@@ -26,7 +28,6 @@ const Accordion = ({ items }) => {
 
   return (<div className="ui styled accordion">
       {renderedItems}
-      <h1>{activeIndex}</h1>
   </div>);
 };
 
